f615b99964b5c21b02eb7465020fbf4b2704d912
Sam <fruxsalomon@gmail.com>
2020-06-11 12:27:33 +0200

    Update HomeController.cs

--- ImageToSql/Controllers/HomeController.cs
+++ ImageToSql/Controllers/HomeController.cs
@@ -85,10 +85,10 @@ namespace UploadToSql.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+      //  public IActionResult Error()
+        //{
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //}
     }
 }
 
